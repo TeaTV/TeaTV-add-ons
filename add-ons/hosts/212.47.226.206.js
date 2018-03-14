1ebb70409dcc846547217ff60e980ca483d1817c
TeaTV <teatv.official@gmail.com>
2018-03-14 11:56:30 +0700

    Add files via upload
    add hosts

---
+++ add-ons/hosts/212.47.226.206.js
@@ -0,0 +1,45 @@
+class FastServer {
+
+    constructor(props) {
+
+        this.libs       = props.libs;
+        this.settings   = props.settings;
+        this.state      = {};
+    }
+
+    convertToEmbed() {
+        
+        // convert link detail to link embed
+        // if input is embed then return input
+    }
+
+    async getLink(url) {
+
+        const { httpRequest, cheerio } = this.libs;
+        
+        let results = [];
+
+        let isDie = 'NOR';
+        try {
+            isDie       = await httpRequest.isLinkDie(url);
+        } catch(error) {}
+    
+        if( isDie != false ) {
+
+            results.push({
+                file: url, label: 'NOR', type: "direct" , size: isDie
+            });
+        }
+
+        return {
+            host: {
+                url: url,
+                name: "Fast-Server"
+            },
+            result: results
+        }
+
+    }
+}
+
+exports.default = (libs, settings) => new FastServer({ libs, settings });
