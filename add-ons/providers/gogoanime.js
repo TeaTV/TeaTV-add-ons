75a82e75674071235ee405fb4c536039cc69a492
TeaTV <teatv.official@gmail.com>
2018-03-19 17:14:42 +0700

    Add files via upload
    update code

--- add-ons/providers/gogoanime.js
+++ add-ons/providers/gogoanime.js
@@ -1,185 +1,117 @@
-
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var URL = {
-    DOMAIN: 'http://www.gogoanime.to',
-    SEARCH: function SEARCH(title) {
-        return 'http://www.gogoanime.to/?s=' + title;
+const URL = {
+    DOMAIN: `http://www.gogoanime.to`,
+    SEARCH: (title, page=false) => {
+        if( page != false ) return `http://www.gogoanime.to/page/${page}?s=${title}`;
+        return `http://www.gogoanime.to/?s=${title}`;
     }
 };
 
-var Gogoanime = function () {
-    function Gogoanime(props) {
-        _classCallCheck(this, Gogoanime);
-
-        this.libs = props.libs;
-        this.movieInfo = props.movieInfo;
-        this.settings = props.settings;
-        this.state = {};
+class Gogoanime {
+    constructor(props) {
+        this.libs       = props.libs;
+        this.movieInfo  = props.movieInfo;
+        this.settings   = props.settings;
+        this.state      = {};
     }
 
-    _createClass(Gogoanime, [{
-        key: 'searchDetail',
-        value: function () {
-            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-                var _libs, httpRequest, cheerio, stringHelper, base64, _movieInfo, title, year, episode, type, detailUrl, urlSearch, htmlSearch, $, itemSearch;
+    async searchDetail() {
 
-                return regeneratorRuntime.wrap(function _callee$(_context) {
-                    while (1) {
-                        switch (_context.prev = _context.next) {
-                            case 0:
-                                _libs = this.libs, httpRequest = _libs.httpRequest, cheerio = _libs.cheerio, stringHelper = _libs.stringHelper, base64 = _libs.base64;
-                                _movieInfo = this.movieInfo, title = _movieInfo.title, year = _movieInfo.year, episode = _movieInfo.episode, type = _movieInfo.type;
-                                detailUrl = false;
-                                urlSearch = URL.SEARCH(stringHelper.convertToSearchQueryString(title, '+') + ('+episode+' + episode));
-                                _context.next = 6;
-                                return httpRequest.getHTML(urlSearch);
+        const { httpRequest, cheerio, stringHelper, base64 } = this.libs; 
+        let { title, year, episode, type } = this.movieInfo;
 
-                            case 6:
-                                htmlSearch = _context.sent;
-                                $ = cheerio.load(htmlSearch);
-                                itemSearch = $('.postlist');
+        let detailUrl       = false;
 
+        let arrPage         = [];
 
-                                itemSearch.each(function () {
+        let urlSearch       = URL.SEARCH(stringHelper.convertToSearchQueryString(title, '+') + `+episode+${episode}`);
+        let htmlSearch      = await httpRequest.getHTML(urlSearch);
+        let $               = cheerio.load(htmlSearch);
 
-                                    var hrefMovie = $(this).find('a').attr('href');
-                                    var titleMovie = $(this).find('a').text();
-                                    var episodeMovie = titleMovie.match(/episode *([0-9]+)/i);
-                                    episodeMovie = episodeMovie != null ? +episodeMovie[1] : -1;
-                                    titleMovie = titleMovie.replace(/ *episode *[0-9]+/i, '').trim();
+        let paginate        = $('.wp-pagenavi .last').attr('href');
+        paginate            = paginate.match(/page\/([0-9]+)/i);
+        paginate            = paginate != null ? +paginate[1] : 1;
 
-                                    if (stringHelper.shallowCompare(titleMovie, title) && episode == episodeMovie) {
-                                        detailUrl = hrefMovie;
-                                        return;
-                                    }
-                                });
+        for( let i = 1; i <= paginate; i++ ) {
+            arrPage.push(i);
+        }
 
-                                this.state.detailUrl = detailUrl;
-                                return _context.abrupt('return');
+        let arrPromise      = arrPage.map(async function(val) {
 
-                            case 12:
-                            case 'end':
-                                return _context.stop();
-                        }
-                    }
-                }, _callee, this);
-            }));
+            urlSearch       = URL.SEARCH(stringHelper.convertToSearchQueryString(title, '+') + `+episode+${episode}`, val);
+            htmlSearch      = await httpRequest.getHTML(urlSearch);
+            $               = cheerio.load(htmlSearch); 
 
-            function searchDetail() {
-                return _ref.apply(this, arguments);
-            }
+            let itemSearch  = $('.postlist');
 
-            return searchDetail;
-        }()
-    }, {
-        key: 'getHostFromDetail',
-        value: function () {
-            var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
-                var _libs2, httpRequest, cheerio, base64, hosts, detailUrl, htmlSearch, $, itemEmbed;
+            itemSearch.each(function() {
 
-                return regeneratorRuntime.wrap(function _callee2$(_context2) {
-                    while (1) {
-                        switch (_context2.prev = _context2.next) {
-                            case 0:
-                                _libs2 = this.libs, httpRequest = _libs2.httpRequest, cheerio = _libs2.cheerio, base64 = _libs2.base64;
+                let hrefMovie   = $(this).find('a').attr('href');
+                let titleMovie  = $(this).find('a').text();
+                let episodeMovie= titleMovie.match(/episode *([0-9]+)/i);
+                episodeMovie    = episodeMovie != null ? +episodeMovie[1] : -1;
+                titleMovie      = titleMovie.replace(/ *episode *[0-9]+/i, '').trim();
 
-                                if (this.state.detailUrl) {
-                                    _context2.next = 3;
-                                    break;
-                                }
-
-                                throw new Error("NOT_FOUND");
-
-                            case 3:
-                                hosts = [];
-                                detailUrl = this.state.detailUrl;
-                                _context2.next = 7;
-                                return httpRequest.getHTML(this.state.detailUrl);
-
-                            case 7:
-                                htmlSearch = _context2.sent;
-                                $ = cheerio.load(htmlSearch);
-                                itemEmbed = $('#content .postcontent iframe');
-
-
-                                itemEmbed.each(function () {
-
-                                    var linkEmbed = $(this).attr('src');
-
-                                    linkEmbed && hosts.push({
-                                        provider: {
-                                            url: detailUrl,
-                                            name: "gogoanime"
-                                        },
-                                        result: {
-                                            file: linkEmbed,
-                                            label: "embed",
-                                            type: "embed"
-                                        }
-                                    });
-                                });
-
-                                this.state.hosts = hosts;
-                                return _context2.abrupt('return');
-
-                            case 13:
-                            case 'end':
-                                return _context2.stop();
-                        }
-                    }
-                }, _callee2, this);
-            }));
-
-            function getHostFromDetail() {
-                return _ref2.apply(this, arguments);
-            }
-
-            return getHostFromDetail;
-        }()
-    }]);
-
-    return Gogoanime;
-}();
-
-thisSource.function = function () {
-    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(libs, movieInfo, settings) {
-        var gogo;
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
-            while (1) {
-                switch (_context3.prev = _context3.next) {
-                    case 0:
-                        gogo = new Gogoanime({
-                            libs: libs,
-                            movieInfo: movieInfo,
-                            settings: settings
-                        });
-                        _context3.next = 3;
-                        return gogo.searchDetail();
-
-                    case 3:
-                        _context3.next = 5;
-                        return gogo.getHostFromDetail();
-
-                    case 5:
-                        return _context3.abrupt('return', gogo.state.hosts);
-
-                    case 6:
-                    case 'end':
-                        return _context3.stop();
+                if( stringHelper.shallowCompare(titleMovie, title) && episode == episodeMovie )  {
+                    detailUrl = hrefMovie;
+                    return;
                 }
-            }
-        }, _callee3, undefined);
-    }));
+            });
+        });
 
-    return function (_x, _x2, _x3) {
-        return _ref3.apply(this, arguments);
-    };
-}();
+        await Promise.all(arrPromise);
 
-thisSource.testing = Gogoanime;
+        this.state.detailUrl = detailUrl;
+        return;
+    }
+
+
+    async getHostFromDetail() {
+
+        const { httpRequest, cheerio, base64 } = this.libs;
+        if(!this.state.detailUrl) throw new Error("NOT_FOUND");
+
+        let hosts = [];
+
+        let detailUrl   = this.state.detailUrl;
+
+        let htmlSearch  = await httpRequest.getHTML(this.state.detailUrl);
+        let $           = cheerio.load(htmlSearch);
+        let itemEmbed   = $('#content .postcontent iframe');
+
+        itemEmbed.each(function() {
+
+            let linkEmbed = $(this).attr('src');
+
+            linkEmbed && hosts.push({
+                provider: {
+                    url: detailUrl,
+                    name: "gogoanime"
+                },
+                result: {
+                    file: linkEmbed,
+                    label: "embed",
+                    type: "embed"
+                }
+            });
+        });
+
+        this.state.hosts = hosts;
+        return;
+    }
+
+}
+
+exports.default = async (libs, movieInfo, settings) => {
+
+    const gogo = new Gogoanime({
+        libs: libs,
+        movieInfo: movieInfo,
+        settings: settings
+    });
+    await gogo.searchDetail();
+    await gogo.getHostFromDetail();
+    return gogo.state.hosts;
+}
+
+
+exports.testing = Gogoanime;
