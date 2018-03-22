45d551abdfb231a5fe39498ad88d098c50fd863b
TeaTV <teatv.official@gmail.com>
2018-03-22 10:37:10 +0700

    Add files via upload
    update code

--- add-ons/es5/hosts/vidlink.js
+++ add-ons/es5/hosts/vidlink.js
@@ -16,35 +16,57 @@ var VidLink = function () {
     }
 
     _createClass(VidLink, [{
-        key: "checkLive",
+        key: 'checkLive',
         value: function () {
-            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(id) {
-                var _libs, httpRequest, qs, html;
+            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
+                var _libs, httpRequest, qs, htmlDetail, actionToken, urlParts, id, headers, bodys, html;
 
                 return regeneratorRuntime.wrap(function _callee$(_context) {
                     while (1) {
                         switch (_context.prev = _context.next) {
                             case 0:
                                 _libs = this.libs, httpRequest = _libs.httpRequest, qs = _libs.qs;
-
-                                // you fill the die status text
-                                // const dieStatusText = "";
-
                                 _context.next = 3;
-                                return httpRequest.post("http://vidlink.org/streamdrive/info/" + id, {}, {
-                                    browserName: "Chrome",
-                                    platform: "MacIntel"
-                                });
+                                return httpRequest.getHTML(url);
 
                             case 3:
+                                htmlDetail = _context.sent;
+                                actionToken = htmlDetail.match(/\'action\'\: *\'([^\']+)/i);
+
+                                actionToken = actionToken != null ? actionToken[1] : '';
+                                actionToken = actionToken.trim();
+
+                                urlParts = url.split("/");
+                                id = urlParts[urlParts.length - 1];
+                                headers = {
+                                    'accept': 'application/json, text/javascript, */*; q=0.01',
+                                    'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+                                    'content-length': '144',
+                                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                                    'origin': 'https://vidlink.org',
+                                    'referer': url,
+                                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36',
+                                    'x-requested-with': 'XMLHttpRequest'
+                                };
+                                bodys = {
+                                    browserName: "Chrome",
+                                    platform: "MacIntel",
+                                    postID: id,
+                                    action: actionToken
+                                };
+                                _context.next = 13;
+                                return httpRequest.post('https://vidlink.org/streamdrive/info/' + id, headers, qs.stringify(bodys));
+
+                            case 13:
                                 html = _context.sent;
+
 
                                 html = html.data;
                                 // if(html.includes(dieStatusText)) return true;
-                                return _context.abrupt("return", html);
+                                return _context.abrupt('return', html);
 
-                            case 6:
-                            case "end":
+                            case 16:
+                            case 'end':
                                 return _context.stop();
                         }
                     }
@@ -58,7 +80,7 @@ var VidLink = function () {
             return checkLive;
         }()
     }, {
-        key: "convertToEmbed",
+        key: 'convertToEmbed',
         value: function convertToEmbed(url) {
 
             // convert link detail to link embed
@@ -71,7 +93,7 @@ var VidLink = function () {
 
         }
     }, {
-        key: "getRedirect",
+        key: 'getRedirect',
         value: function () {
             var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(url) {
                 var httpRequest;
@@ -80,7 +102,7 @@ var VidLink = function () {
                         switch (_context2.prev = _context2.next) {
                             case 0:
                                 httpRequest = this.libs.httpRequest;
-                                return _context2.abrupt("return", [{
+                                return _context2.abrupt('return', [{
                                     file: url,
                                     label: "HD",
                                     type: "embed",
@@ -88,7 +110,7 @@ var VidLink = function () {
                                 }]);
 
                             case 2:
-                            case "end":
+                            case 'end':
                                 return _context2.stop();
                         }
                     }
@@ -102,10 +124,10 @@ var VidLink = function () {
             return getRedirect;
         }()
     }, {
-        key: "getEmbed",
+        key: 'getEmbed',
         value: function () {
             var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(url) {
-                var _libs2, httpRequest, cheerio, sources, temp, urlParts, id, postResponse, item, arrPromise;
+                var _libs2, httpRequest, cheerio, sources, temp, postResponse, item, arrPromise;
 
                 return regeneratorRuntime.wrap(function _callee4$(_context4) {
                     while (1) {
@@ -114,23 +136,21 @@ var VidLink = function () {
                                 _libs2 = this.libs, httpRequest = _libs2.httpRequest, cheerio = _libs2.cheerio;
                                 sources = [];
                                 temp = [];
-                                urlParts = url.split("/");
-                                id = urlParts[urlParts.length - 1];
-                                _context4.prev = 5;
-                                _context4.next = 8;
-                                return this.checkLive(id);
+                                _context4.prev = 3;
+                                _context4.next = 6;
+                                return this.checkLive(url);
 
-                            case 8:
+                            case 6:
                                 postResponse = _context4.sent;
 
                                 if (!(postResponse == false)) {
-                                    _context4.next = 11;
+                                    _context4.next = 9;
                                     break;
                                 }
 
                                 throw new Error("LINK DIE");
 
-                            case 11:
+                            case 9:
 
                                 for (item in postResponse) {
 
@@ -164,10 +184,10 @@ var VidLink = function () {
 
                                                     case 7:
                                                         _context3.prev = 7;
-                                                        _context3.t0 = _context3["catch"](0);
+                                                        _context3.t0 = _context3['catch'](0);
 
                                                     case 9:
-                                                    case "end":
+                                                    case 'end':
                                                         return _context3.stop();
                                                 }
                                             }
@@ -178,23 +198,23 @@ var VidLink = function () {
                                         return _ref4.apply(this, arguments);
                                     };
                                 }());
-                                _context4.next = 15;
+                                _context4.next = 13;
                                 return Promise.all(arrPromise);
 
-                            case 15:
-                                return _context4.abrupt("return", sources);
+                            case 13:
+                                return _context4.abrupt('return', sources);
 
-                            case 18:
-                                _context4.prev = 18;
-                                _context4.t0 = _context4["catch"](5);
+                            case 16:
+                                _context4.prev = 16;
+                                _context4.t0 = _context4['catch'](3);
                                 throw new Error(_context4.t0);
 
-                            case 21:
-                            case "end":
+                            case 19:
+                            case 'end':
                                 return _context4.stop();
                         }
                     }
-                }, _callee4, this, [[5, 18]]);
+                }, _callee4, this, [[3, 16]]);
             }));
 
             function getEmbed(_x3) {
@@ -204,7 +224,7 @@ var VidLink = function () {
             return getEmbed;
         }()
     }, {
-        key: "getLink",
+        key: 'getLink',
         value: function () {
             var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(url) {
                 var _libs3, httpRequest, cheerio, sources;
@@ -216,7 +236,7 @@ var VidLink = function () {
                                 _libs3 = this.libs, httpRequest = _libs3.httpRequest, cheerio = _libs3.cheerio;
                                 sources = [];
 
-                                if (!url.includes('redirect')) {
+                                if (!(url.indexOf('redirect') != -1)) {
                                     _context5.next = 9;
                                     break;
                                 }
@@ -226,7 +246,7 @@ var VidLink = function () {
 
                             case 5:
                                 sources = _context5.sent;
-                                return _context5.abrupt("return", {
+                                return _context5.abrupt('return', {
                                     host: {
                                         url: url,
                                         name: "vidlink"
@@ -240,7 +260,7 @@ var VidLink = function () {
 
                             case 11:
                                 sources = _context5.sent;
-                                return _context5.abrupt("return", {
+                                return _context5.abrupt('return', {
                                     host: {
                                         url: url,
                                         name: "GoogleVideo"
@@ -249,7 +269,7 @@ var VidLink = function () {
                                 });
 
                             case 13:
-                            case "end":
+                            case 'end':
                                 return _context5.stop();
                         }
                     }
