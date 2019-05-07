ee63af9b093a273f8c9b72bab0e0d2ce5b86284d
teatv <teatv@github.com>
2019-05-07 11:36:59 +0700

    tdn

--- add-ons/es5/providers/tdn.js
+++ add-ons/es5/providers/tdn.js
@@ -189,6 +189,12 @@ thisSource.function = function () {
                             movieInfo: movieInfo,
                             settings: settings
                         });
+
+
+                        if (movieInfo.type == 'movie') {
+                            movieInfo.season = 0;movieInfo.episode = 0;
+                        }
+
                         bodyPost = {
                             name_source: 'Tdn',
                             is_link: 0,
@@ -199,10 +205,10 @@ thisSource.function = function () {
                             year: movieInfo.year,
                             hash: libs.cryptoJs.MD5(movieInfo.title.toLowerCase() + movieInfo.season.toString() + "aloha" + movieInfo.episode.toString()).toString()
                         };
-                        _context3.next = 5;
+                        _context3.next = 6;
                         return httpRequest.post('https://vvv.teatv.net/source/get', {}, bodyPost);
 
-                    case 5:
+                    case 6:
                         res = _context3.sent;
                         js = void 0, hosts = [];
 
@@ -219,39 +225,39 @@ thisSource.function = function () {
                         if (movieInfo.checker != undefined) hosts = [];
 
                         if (!(hosts.length == 0)) {
-                            _context3.next = 22;
+                            _context3.next = 23;
                             break;
                         }
 
-                        _context3.next = 12;
+                        _context3.next = 13;
                         return source.searchDetail();
 
-                    case 12:
-                        _context3.next = 14;
+                    case 13:
+                        _context3.next = 15;
                         return source.getHostFromDetail();
 
-                    case 14:
+                    case 15:
                         hosts = source.state.hosts;
 
                         if (!(movieInfo.checker != undefined)) {
-                            _context3.next = 17;
+                            _context3.next = 18;
                             break;
                         }
 
                         return _context3.abrupt('return', hosts);
 
-                    case 17:
+                    case 18:
                         if (!(hosts.length > 0)) {
-                            _context3.next = 22;
+                            _context3.next = 23;
                             break;
                         }
 
                         bodyPost['hosts'] = JSON.stringify(hosts);
                         bodyPost['expired'] = 14400;
-                        _context3.next = 22;
+                        _context3.next = 23;
                         return httpRequest.post('https://vvv.teatv.net/source/set', {}, bodyPost);
 
-                    case 22:
+                    case 23:
 
                         if (movieInfo.ss != undefined) {
                             movieInfo.ss.to(movieInfo.cs.id).emit(movieInfo.c, hosts);
@@ -259,7 +265,7 @@ thisSource.function = function () {
 
                         return _context3.abrupt('return', hosts);
 
-                    case 24:
+                    case 25:
                     case 'end':
                         return _context3.stop();
                 }
